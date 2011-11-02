9cf79f1109160c3b955bffa9ddb3a7ee209e3d57
Henning Huncke <hh@i22.de>
2011-11-02 17:18:30 +0100

    finishing the effect "sliceUp"

--- javascripts/jquery.simpleSlider.effects.js
+++ javascripts/jquery.simpleSlider.effects.js
@@ -325,40 +325,49 @@
         function (current, next, opts) {
             var options     = $.extend({}, this.getOptions(), opts, {squaresPerHeight: 1});
             var self        = this;
+            var spw         = options.squaresPerWidth;
+            var sph         = options.squaresPerHeight;
+            var callback    = null;
             
+            $('img:first', next).hide();
             $(next).css({
                 'top': 0,
                 'left': 0,
-                'z-index': options.zIndex + 90
+                'z-index': options.zIndex + 100
             }).show();
-            $(current).css('z-index', options.zIndex + 100);
+            $(current).css('z-index', options.zIndex + 90);
             
             var dimension = $.simpleSlider.buildSquareMatrix(next, options);
-            $('div:[id*="simpleSlider-square"]', next).css('top', 0);
+            $('div:[id*="simpleSlider-square"]', next).css('top', options.height);
             
-            $('img:first', next).hide();
-            $(next).css('z-index', options.zIndex + 101);
-            
-            $('div:[id*="simpleSlider-square"]', next).each(function () {
-                var row = parseInt(this.id.substr(this.id.indexOf('-', 19)+1));
+            var count = $('div:[id*="simpleSlider-square"]', next).length;
+            $('div:[id*="simpleSlider-square"]', next).each(function (index) {
                 var col = parseInt(this.id.substr(this.id.lastIndexOf('-')+1));
-                var wait = (row + col) - 1;
+                var wait = col - 1;
                 
                 if (opts.direction == '+') {
                     wait = (spw + sph) - wait;
+                    count = 1;
                 }
-                wait *= options.speed / (spw + sph - 1);
-            });
-            
-            $('div:[id*="simpleSlider-square"]', current).show().animate({
-                'width': 0,
-                'margin-left': margin
-            }, {
-                duration: options.speed,
-                complete: function () {
-                    $('div:[id*="simpleSlider-square-"]', current).remove();
-                    self.complete();
+                wait *= options.speed / (options.squaresPerWidth * 5);
+                
+                if (index == (count - 1)) {
+                    callback = function () {
+                        $('div:[id*="simpleSlider-square-"]', next).remove();
+                        $('img:first', current).hide();
+                        $('img:first', next).show();
+                        self.complete();
+                    }
+                } else {
+                    callback = null;
                 }
+                
+                $(this).delay(wait).animate({
+                    'top': 0
+                }, {
+                    duration: options.speed / options.squaresPerWidth,
+                    complete: callback
+                });
             });
         } 
     );
