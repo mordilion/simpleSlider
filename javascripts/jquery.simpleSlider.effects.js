1280254209475af976b92a4c2f37c7a06231cb1e
Henning Huncke <hh@i22.de>
2011-11-02 15:21:35 +0100

    adding new effect "rainGrow" and fixing some bugs on rain based effects

--- javascripts/jquery.simpleSlider.effects.js
+++ javascripts/jquery.simpleSlider.effects.js
@@ -1,7 +1,7 @@
 /**
  * effects for simpleSlider
  *
- * @version: 1.1.0 - (2011/09/22)
+ * @version: 1.1.1 - (2011/11/02)
  * @author Henning Huncke
  *
  * Copyright (c) 2011 Henning Huncke (http://www.devjunkie.de)
@@ -33,6 +33,35 @@
         }
         
         return new Array(squareWidth, squareHeight);
+    };
+    
+    // method for animate the css value for background-position
+    $.simpleSlider.animateBackgroundPosition = function (fx) {
+	    if (!fx.set) {
+		    var position = $(fx.elem).css('background-position') || '0 0';
+		    $(fx.elem).css('background-position', position);
+		    fx.start = $.simpleSlider.parseBackgroundPosition(position);
+		    
+		    position = $.fn.jquery >= '1.6' ? fx.end : fx.options.curAnim['backgroundPosition'] || fx.options.curAnim['background-position'];
+		    fx.end = $.simpleSlider.parseBackgroundPosition(position);
+		    
+		    fx.set = true;
+	    }
+
+	    $(fx.elem).css(
+	        'background-position',
+	        (fx.start[0] + (fx.pos * (fx.end[0] - fx.start[0]))) + 'px ' + (fx.start[1] + (fx.pos * (fx.end[1] - fx.start[1]))) + 'px'
+		);
+    };
+    
+    // method to parse the given background-position value on animate
+    $.simpleSlider.parseBackgroundPosition = function (value) {
+        var position = value.split(/ /);
+        
+        position[0] = parseFloat(position[0]);
+        position[1] = parseFloat(position[1]);
+
+        return position;
     };
     
     // overlay horizontal slide effect
@@ -134,7 +163,7 @@
             $(current).css('z-index', options.zIndex + 100);
             
             var dimension = $.simpleSlider.buildSquareMatrix(current, options);
-            margin = opts.direction == '+' ? dimension[0] : 0;
+            margin += opts.direction == '+' ? dimension[0] : 0;
             
             $('img:first', current).hide();
             $('div:[id*="simpleSlider-square"]', current).show().animate({
@@ -165,7 +194,7 @@
             $(current).css('z-index', options.zIndex + 100);
             
             var dimension = $.simpleSlider.buildSquareMatrix(current, options);
-            margin = opts.direction == '+' ? dimension[1] : 0;
+            margin += opts.direction == '+' ? dimension[1] : 0;
             
             $('img:first', current).hide();
             $('div:[id*="simpleSlider-square"]', current).show().animate({
@@ -207,14 +236,87 @@
                 var wait = (row + col) - 1;
                 if (opts.direction == '+') {
                     wait = (spw + sph) - wait;
+                    count = 1;
                 }
                 wait *= options.speed / (spw + sph - 1);
-                
+
                 if (index == (count - 1)) {
-                    callback = self.complete;
+                    callback = function () {
+                        $('div:[id*="simpleSlider-square-"]', current).remove();
+                        self.complete();
+                    }
+                } else {
+                    callback = null;
                 }
 
                 $(this).delay(wait).fadeOut(Math.floor(options.speed / 4), callback);
+            });
+        }
+    );
+    
+    // rain-grow effect
+    $.simpleSlider.addEffect('rainGrow',
+        function (current, next, opts) {
+            var options     = $.extend({}, this.getOptions(), opts);
+            var self        = this;
+            var spw         = options.squaresPerWidth;
+            var sph         = options.squaresPerHeight;
+            var callback    = null;
+            
+            $(next).css({
+                'top': 0,
+                'left': 0,
+                'z-index': options.zIndex + 90
+            }).show();
+            $(current).css('z-index', options.zIndex + 100);
+            
+            var dimension = $.simpleSlider.buildSquareMatrix(current, options);
+            var marginLeft = opts.direction != '+' ? dimension[0] : 0;
+            var marginTop = opts.direction != '+' ? dimension[1] : 0;
+            var backgroundPosition = '0 0';
+            $('img:first', current).hide();
+            
+            var count = $('div:[id*="simpleSlider-square"]', current).length;
+            $('div:[id*="simpleSlider-square"]', current).each(function (index) {
+                var row = parseInt(this.id.substr(this.id.indexOf('-', 19)+1));
+                var col = parseInt(this.id.substr(this.id.lastIndexOf('-')+1));
+                var wait = (row + col) - 1;
+                
+                if (opts.direction == '+') {
+                    wait = (spw + sph) - wait;
+                    count = 1;
+                    backgroundPosition = $(this).css('background-position');
+                } else {
+                    backgroundPosition = '-' + ((col) * dimension[0]) + 'px -' + ((row) * dimension[1]) + 'px';
+                }
+                console.log(backgroundPosition);
+                wait *= options.speed / (spw + sph - 1);
+                
+                if (index == (count - 1)) {
+                    callback = function () {
+                        $('div:[id*="simpleSlider-square-"]', current).remove();
+                        self.complete();
+                    }
+                } else {
+                    callback = null;
+                }
+                
+                $(this).delay(wait).animate({
+                    'height': 0,
+                    'width': 0,
+                    'margin-left': marginLeft,
+                    'margin-top': marginTop,
+                    'backgroundPosition': backgroundPosition
+                }, {
+                    duration: options.speed / 4,
+                    step: function (now, fx) {
+                        if (fx.prop == 'backgroundPosition' || fx.prop == 'background-position') {
+                            $.simpleSlider.animateBackgroundPosition(fx);
+                        }
+                    },                    
+                    complete: callback
+                });
+
             });
         }
     );
