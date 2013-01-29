af9ed2de16ced602fa7f385112eeba255028181e
Henning Huncke <henning.huncke@llg-media.de>
2013-01-29 19:49:14 +0100

    fixed bug for newer versions of jquery

--- javascripts/jquery.simpleSlider.js
+++ javascripts/jquery.simpleSlider.js
@@ -10,21 +10,21 @@
  * Built for jQuery library
  * http://jquery.com
  */
- 
+
 
 (function($) {
 
-    /** 
+    /**
      * static constructs
      ********************************************************************************/
     $.simpleSlider = {
         version: '1.4.19',
-    
+
         addEffect: function (name, fn) {
             effects[name] = fn;
             effectNames.push(name);
         },
-        
+
         defaults: {
             height: 260,                // height of the slider panel
             width: 600,                 // width of the slider panel
@@ -41,7 +41,7 @@
             titleOpacity: 0.7,          // opacity of the title
             titleSpeed: 500,            // speed of title fade in ms
             useImageTitle: false,       // use the value of image-title attribute to display title
-            
+
             hoverPause: true,           // pause slide on hover
             navigation: true,           // show previous, next and buttons
             numeric: false,             // show the buttons as numeric
@@ -51,13 +51,13 @@
         }
     };
     /********************************************************************************/
-    
-    
+
+
     var instances   = [];
     var effects     = [];
     var effectNames = [];
-    
-    
+
+
     /**
      * default effects
      ********************************************************************************/
@@ -66,12 +66,12 @@
         function (current, next, opts) {
             var name = opts.last;
             var effect = null;
-            
+
             do {
                 var rand = Math.floor(Math.random() * effectNames.length);
                 name = effectNames[rand];
             } while(opts.last === name || name === 'random');
-            
+
             effect = effects[name];
             if (effect) {
                 effect.call(this, current, next, opts);
@@ -79,12 +79,12 @@
             return name;
         }
     );
-     
+
     // the simple horizontal slide effect
     $.simpleSlider.addEffect('horizontal',
         function (current, next, opts) {
             var options = this.getOptions();
-        
+
             next.css({
                 'top': 0,
                 'left': parseInt(opts.direction + options.width) * -1,
@@ -92,7 +92,7 @@
             }).show().animate({
                 'left': opts.direction + '=' + options.width
             }, options.speed);
-            
+
             current.css({
                 'z-index': options.zIndex + 90
             }).animate({
@@ -103,12 +103,12 @@
             });
         }
     );
-    
+
     // the simple vertical slide effect
     $.simpleSlider.addEffect('vertical',
         function (current, next, opts) {
             var options = this.getOptions();
-        
+
             next.css({
                 'top': parseInt(opts.direction + options.height) * -1,
                 'left': 0,
@@ -116,7 +116,7 @@
             }).show().animate({
                 'top': opts.direction + '=' + options.height
             }, options.speed);
-            
+
             current.css({
                 'z-index': options.zIndex + 90
             }).animate({
@@ -127,28 +127,28 @@
             });
         }
     );
-    
+
     // the simple fade effect
     $.simpleSlider.addEffect('fade',
         function (current, next, opts) {
             var options = this.getOptions();
-            
+
             current.css({
                 'z-index': options.zIndex + 100
             });
-            
+
             next.css({
                 'top': 0,
                 'left': 0,
                 'z-index': options.zIndex + 90
             }).show();
-            
+
             current.fadeOut(options.speed, this.complete);
         }
     );
     /********************************************************************************/
-    
-    
+
+
     /**
      * slider object
      ********************************************************************************/
@@ -157,26 +157,26 @@
         var list      = $('ul:first', element);
         var wrapper   = null;
         var timeout   = null;
-        
+
         var count     = $('li', list).length;
         var index     = 0;
-        
+
         var last      = '';
 
-        var clicked   = false;        
+        var clicked   = false;
         var slideable = true;
         var pause     = false;
-        
-        
+
+
         /**
          * initialization needed elements
          */
         function init() {
             // get the theme-class
             var classes = $(element).attr('class');
-            classes = classes != undefined ? classes.split(/ /) : null; 
+            classes = classes != undefined ? classes.split(/ /) : null;
             var theme = null;
-            
+
             if (classes != null) {
                 $(classes).each(function (index, value) {
                     if (value.substring(0, 5) === 'theme') {
@@ -186,16 +186,16 @@
                     }
                 });
             }
-        
+
             // setup the panel
             $(element).css({
-                'height': options.height, 
-                'width': options.width, 
-                'overflow': 'hidden', 
+                'height': options.height,
+                'width': options.width,
+                'overflow': 'hidden',
                 'position': 'relative'
             }).wrap('<div class="simpleSlider' + (theme !== null ? ' ' + theme : '') + '" id="simpleSlider-' + element.id + '" />');
             wrapper = $('#simpleSlider-' + element.id);
-                
+
             // setup all list elements
             $('li', list).css({
                 'position': 'absolute',
@@ -208,12 +208,12 @@
             getElement(index).css({
                 'z-index': options.zIndex + 110
             });
-            
+
             // call other init-methods
             initTitle();
         };
-        
-        
+
+
         /**
          * initialization the title elements
          */
@@ -221,23 +221,23 @@
             // setup the title main element
             $(element).append('<div class="simpleSlider-title" id="simpleSlider-' + element.id + '-title" />');
             var title = getTitle();
-            
+
             // append the transparency and content elements
             $(title).append('<div class="simpleSlider-title-transparency" id="simpleSlider-' + element.id + '-title-transparency" />');
             $(title).append('<div class="simpleSlider-title-content" id="simpleSlider-' + element.id + '-title-content" />');
-            
+
             var transparency = getTitleTransparency();
-            
+
             // style the elements
             $(title).css({
-                'bottom': 0, 
+                'bottom': 0,
                 'display': 'block',
-                'left': 0, 
+                'left': 0,
                 'width': options.width - parseInt($(title).css('padding-left')) - parseInt($(title).css('padding-right')),
                 'position': 'absolute',
                 'z-index': options.zIndex + 120
             }).hide();
-            
+
             $(transparency).css({
                 'display': 'block',
                 'height': '100%',
@@ -249,8 +249,8 @@
                 'z-index': -1
             });
         };
-        
-        
+
+
         /**
          * initialization the navigation elements
          */
@@ -258,47 +258,47 @@
             // setup navigation buttons
             $(wrapper).append('<ol class="simpleSlider-navigation-buttons" id="simpleSlider-' + element.id + '-navigation-buttons" />');
             var buttons = getNavigationButtons();
-            
+
             $(buttons).css({'z-index': options.zIndex + 120});
             for (var i = 1; i <= count; i++) {
                 var li = $(buttons).append('<li />').find('li:last');
                 var link = options.numeric ? i : '';
                 $(li).append('<a class="simpleSlider-navigation-button" id="simpleSlider-' + element.id + '-navigation-button-' + i + '" href="javascript:void(0);" rel="' + (i - 1) + '">' + link + '</a>');
             }
-            
+
             // setup navigation (prev, next)
             $(element).append('<div class="simpleSlider-navigation" id="simpleSlider-' + element.id + '-navigation" style="position: absolute" />');
             var navigation = getNavigation();
-            
+
             $(navigation).css({'width': options.width, 'height': options.height, 'z-index': options.zIndex + 110})
                 .append('<a class="simpleSlider-navigation-previous" id="simpleSlider-' + element.id + '-navigation-previous" href="javascript:void(0);" rel="prev">' + options.previousText + '</a>')
                 .append('<a class="simpleSlider-navigation-next" id="simpleSlider-' + element.id + '-navigation-next" href="javascript:void(0);" rel="next">' + options.nextText + '</a>');
-                
-            $('a:[class*="simpleSlider-navigation"]', $(wrapper)).click(function () {
+
+            $('a[class*="simpleSlider-navigation"]', $(wrapper)).click(function () {
                 clearTimeout(timeout);
                 slide($(this).attr('rel'), true);
             });
-            
+
             $(getNavigation(true) + ', ' + getTitle(true)).mouseover(function () {
                 if (options.hoverPause) {
                     clearTimeout(timeout);
                     pause = true;
                 }
-                
+
                 $(getButtonPrevious(true) + ', ' + getButtonNext(true)).show();
             }).mouseout(function () {
                 pause = false;
                 $(getButtonPrevious(true) + ', ' + getButtonNext(true)).hide();
                 resetTimeout();
             });
-            
+
             $(getButtonPrevious(true) + ', ' + getButtonNext(true)).hide();
         };
-        
-        
+
+
         /**
          * returns the next button element
-         */ 
+         */
         function getButtonNext(asSelector) {
             var selector = '#simpleSlider-' + element.id + '-navigation-next';
             if (asSelector) {
@@ -306,11 +306,11 @@
             }
             return $(selector, wrapper);
         };
-        
-        
+
+
         /**
          * returns the previous button element
-         */ 
+         */
         function getButtonPrevious(asSelector) {
             var selector = '#simpleSlider-' + element.id + '-navigation-previous';
             if (asSelector) {
@@ -318,16 +318,16 @@
             }
             return $(selector, wrapper);
         };
-        
-        
+
+
         /**
          * returns the element by given index
          */
         function getElement(i) {
             return $('li:eq(' + i + ')', $('ul:first', element));
         };
-        
-        
+
+
         /**
          * returns the index by given modifier
          */
@@ -337,11 +337,11 @@
             }
             return (index + modifier) < 0 ? count - 1 : (index + modifier) >= count ? 0 : (index + modifier);
         };
-        
-        
+
+
         /**
          * returns the navigation element (prev, next)
-         */ 
+         */
         function getNavigation(asSelector) {
             var selector = '#simpleSlider-' + element.id + '-navigation';
             if (asSelector) {
@@ -349,11 +349,11 @@
             }
             return $(selector, wrapper);
         };
-        
+
 
         /**
          * returns the navigation buttons element
-         */        
+         */
         function getNavigationButtons(asSelector) {
             var selector = '#simpleSlider-' + element.id + '-navigation-buttons';
             if (asSelector) {
@@ -361,8 +361,8 @@
             }
             return $(selector, wrapper);
         };
-        
-        
+
+
         /**
          * returns the title element or the selector
          */
@@ -373,8 +373,8 @@
             }
             return $(selector, wrapper);
         };
-        
-        
+
+
         /**
          * returns the title-trancparency element or the selector
          */
@@ -385,8 +385,8 @@
             }
             return $(selector, getTitle());
         };
-        
-        
+
+
         /**
          * returns the title-content element or the selector
          */
@@ -397,8 +397,8 @@
             }
             return $(selector, getTitle());
         };
-        
-        
+
+
         /**
          * reset the timeout
          */
@@ -406,11 +406,11 @@
             if (pause) {
                 return;
             }
-        
+
             if (timeout !== null) {
                 clearTimeout(timeout);
             }
-            
+
             if (!clicked || options.keepSlide) {
                 if (options.auto) {
                     timeout = setTimeout(function () {
@@ -419,8 +419,8 @@
                 }
             }
         };
-        
-        
+
+
         /**
          * reset the title element
          */
@@ -428,7 +428,7 @@
             if (!options.title) {
                 return;
             }
-    
+
             var text = '';
             if (options.useImageTitle) {
                 text = $('img:first', getElement(index)).attr('title');
@@ -439,7 +439,7 @@
             if (text == null) {
                 text = '';
             }
-            
+
             getTitle().fadeOut(function () {
                 if (text.length !== 0) {
                     getTitleContent().html(text);
@@ -447,8 +447,8 @@
                 }
             });
         };
-        
-        
+
+
         /**
          * reset the navigation
          */
@@ -457,24 +457,24 @@
             $('a', buttons).removeClass('current');
             $('li:eq(' + index + ') a', buttons).addClass('current');
         };
-        
-        
+
+
         /**
          * reset all list elements
          */
         function resetListElements(nextIndex) {
             $('img:first', getElement(index)).show();
-            
+
             for (var i = 0; i < count; i++) {
                 if (i === index || i === nextIndex) {
                     continue;
                 }
-                
+
                 getElement(i).css('z-index', options.zIndex);
             }
         };
-    
-        
+
+
         /**
          * starts the slide
          */
@@ -486,25 +486,25 @@
             var direction = '-';
             var current   = null;
             var next      = null;
-            
+
             if (!slideable) {
                 return;
             }
-            
+
             slideable = false;
             clicked = click;
-            
+
             switch (to) {
                 case 'auto':
-                case 'next': 
+                case 'next':
                     nextIndex = getIndex(1);
                     break;
-                
+
                 case 'prev':
                     nextIndex = getIndex(-1);
                     direction = '+';
                     break;
-                    
+
                 default:
                     nextIndex = parseInt(to);
                     nextIndex = nextIndex < count && nextIndex >= 0 ? nextIndex : 0;
@@ -513,13 +513,13 @@
                     }
                     break;
             }
-            
+
             if (index !== nextIndex) {
                 current = getElement(index);
                 next = getElement(nextIndex);
                 index = nextIndex;
                 $.extend(opts, {direction: direction});
-                
+
                 if ($.isArray(options.effect)) {
                     if (options.effect.length > 1) {
                         do {
@@ -538,12 +538,12 @@
                 }
 
                 var effect = effects[name];
-                
+
                 if (effect) {
                     resetListElements();
                     resetNavigation();
                     var result = effect.call(self, current, next, opts);
-                    
+
                     if (options.effect === 'random') {
                         last = result;
                     }
@@ -553,8 +553,8 @@
                 slideable = true;
             }
         };
-        
-        
+
+
         // API methods
         $.extend(self, {
             /**
@@ -564,7 +564,7 @@
                 slide('prev', true);
                 return self;
             },
-            
+
             /**
              * public method to slide to next
              */
@@ -572,7 +572,7 @@
                 slide('next', true);
                 return self;
             },
-            
+
             /**
              * public method to restart the slider
              */
@@ -590,7 +590,7 @@
                 resetTimeout(clicked);
                 slideable = true;
             },
-            
+
             /**
              * public method to finishing the slide (must be called after the slide is finished)
              */
@@ -599,7 +599,7 @@
                 resetTimeout(clicked);
                 slideable = true;
             },
-            
+
             /**
              * public method to get current options
              */
@@ -607,46 +607,46 @@
                 return options;
             }
         });
-        
+
         index = options.index < 0 ? count - 1 : options.index >= count ? 0 : options.index;
         options.index = index;
-  
+
         init();
         if (options.navigation) {
             initNavigation();
         }
-        
+
         resetNavigation();
         resetTitle(getElement(index));
         resetListElements();
-        
+
         if (options.auto) {
             resetTimeout();
         }
     };
     /********************************************************************************/
-    
-    
+
+
     /**
      * jQuery plugin initialization
      ********************************************************************************/
     $.fn.simpleSlider = function (options) {
         var element = this;
         var dataName = 'simpleSlider';
-        
+
         // if already constructed the return it
         if (element.data(dataName)) {
             return element.data(dataName);
         }
-        
+
         options = $.extend(true, {}, $.simpleSlider.defaults, options);
-        
+
         this.each(function () {
             element = new SimpleSlider(this, options);
             instances.push(element);
             $(this).data(dataName, element);
         });
-        
+
         return options.api ? element : this;
     };
     /********************************************************************************/
